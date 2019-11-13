0ff8e22489d1beeb33fb122e127eed375efac71e
jonastoussaint <55105660+jonastoussaint@users.noreply.github.com>
2019-11-12 20:11:18 -0500

    Add files via upload

---
+++ TasteBuds2/app/src/main/java/com/example/tastebuds/MainActivity.java
@@ -0,0 +1,14 @@
+epackage com.example.tastebuds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
